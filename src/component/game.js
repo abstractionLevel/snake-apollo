20711e8d7f31d0967efd78c6ac56c99642651132
ugo <ugodelucia@gmail.com>
2023-10-27 00:06:34 +0200

    refactored position square

--- src/component/game.js
+++ src/component/game.js
@@ -1,127 +1,181 @@
-            import { useEffect, useState } from 'react';
-            import './game.css';
-            import Square from './square';
-            import Food from './food';
+import { useEffect, useState } from 'react';
+import './game.css';
+import Square from './square';
+import Food from './food';
 
-            const Game = () => {
+const Game = () => {
 
-                const [positionSquareX, setPositionSquareX] = useState(10);
-                const [positionSquareY, setPositionSquareY] = useState(0);
-                const [positionFoodX,setPositionFoodX] = useState(100);
-                const [positionFoodY,setPositionFoodY] = useState(0);
-                const [keyUp, setKeyUp] = useState(false);
-                const [keyDown, setKeyDown] = useState(false);
-                const [keyLeft, setKeyLeft] = useState(false);
-                const [keyRight, setKeyRight] = useState(false);
-                const [stopMove,setStopMove] = useState(false);
-                const [squareSize,setSquareSize] = useState(24);
-                const [foodSize,setFoodSize] = useState(12);
-                const [isCollidedWidhFood,setIsCollidedWidthFood] = useState(false);
-                const [countFood, setCountFood] = useState(0);
+    const [positionSquareX, setPositionSquareX] = useState(10);
+    const [positionSquareY, setPositionSquareY] = useState(0);
+    const [positionFoodX, setPositionFoodX] = useState(100);
+    const [positionFoodY, setPositionFoodY] = useState(0);
+    const [keyUp, setKeyUp] = useState(false);
+    const [keyDown, setKeyDown] = useState(false);
+    const [keyLeft, setKeyLeft] = useState(false);
+    const [keyRight, setKeyRight] = useState(false);
+    const [stopMove, setStopMove] = useState(false);
+    const [squareSize, setSquareSize] = useState(24);
+    const [foodSize, setFoodSize] = useState(12);
+    const [isCollidedWidhFood, setIsCollidedWidthFood] = useState(false);
+    const [countFood, setCountFood] = useState(0);
+    const [squareList, setSquareList] = useState([{ x: 20, y: 20, playable: true }]);
 
-                console.log("posizione Square x ", positionSquareX  , " position square y " , positionSquareY);
-                console.log("posizione di food x ", positionFoodX, " position food y ", positionFoodY)
+    const squareIsCollidedWithFood = () => {
+        
+        squareList.forEach(el=>{
+            if(el.playable) {
+                if (el.x + squareSize > positionFoodX &&
+                    el.x < positionFoodX + foodSize &&
+                    el.y < positionFoodY + foodSize &&
+                    el.y + squareSize > positionFoodY) {
+                    setIsCollidedWidthFood(true)
+                    setCountFood(prev => prev + 1);
+                    // addSquare();
+                }
+            }
+        })
+       
+    }
 
-                
-                //quando collido con il food, lo snake deve crescere 
+    const updatePositionBodySquare = (x, y) => {
+        console.log(squareList.length)
 
-                const squareIsCollidedWithFood = () => {
-                    if(positionSquareX + squareSize > positionFoodX &&
-                        positionSquareX  < positionFoodX + foodSize &&
-                        positionSquareY < positionFoodY + foodSize &&
-                        positionSquareY + squareSize > positionFoodY) {
-                            setIsCollidedWidthFood(true)
-                            console.log("COLLISIONE")
+        if (squareList.length > 0) {
+            const newList = squareList.map(val => {
+                return { ...val, x: val.x + x, y: val.y + y }
+            })
+        }
+    }
+
+    const addSquare = () => {
+        setSquareList(prev => {
+            let newSquare = {}
+            if (keyDown) {
+                newSquare = { x: positionSquareX, y: positionSquareY - 20, playable: false }
+            }
+            return [...prev, newSquare];
+        });
+    }
+    const handleKeyDown = (event) => {
+        const directionMapping = {
+            ArrowUp: 'up',
+            ArrowDown: 'down',
+            ArrowLeft: 'left',
+            ArrowRight: 'right'
+        }
+        const direction = directionMapping[event.key];
+
+        if (direction) {
+            setKeyUp(direction === 'up');
+            setKeyDown(direction === 'down');
+            setKeyRight(direction === 'right');
+            setKeyLeft(direction === 'left');
+        }
+    };
+
+    const getRandomNumber = (min, max) => {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+
+    const genenerateRandomPositionFood = (min, max) => {
+        let x = getRandomNumber(min, max);
+        let y = getRandomNumber(min, max);
+        while (x === positionSquareX && y === positionSquareY) {
+            x = getRandomNumber(min, max);
+            y = getRandomNumber(min, max);
+        }
+        setPositionFoodX(x);
+        setPositionFoodY(y);
+    }
+
+    useEffect(() => {
+        if (isCollidedWidhFood) {
+            genenerateRandomPositionFood(110, 200);
+            setIsCollidedWidthFood(false);
+
+        }
+    }, [isCollidedWidhFood]);
+
+    useEffect(() => {
+
+        if (stopMove) {
+            return () => clearInterval(intervalId && intervalId);
+        }
+        const intervalId = setInterval(() => {
+            if (keyDown) {
+                setSquareList(prevElementi => {
+                    return prevElementi.map(el => {
+                        if(el.playable) {
+                            return  el.y < 666 ? { ...el, y: el.y + 10 } : { ...el, y: el.y };
                         }
-                }
-
-                const handleKeyDown = (event) => {
-                    const directionMapping = {
-                        ArrowUp: 'up',
-                        ArrowDown: 'down',
-                        ArrowLeft: 'left',
-                        ArrowRight: 'right'
-                    }
-                    const direction = directionMapping[event.key];
-
-                    if (direction) {
-                        setKeyUp(direction === 'up');
-                        setKeyDown(direction === 'down');
-                        setKeyRight(direction === 'right');
-                        setKeyLeft(direction === 'left');
-                    }
-                };
-
-                const getRandomNumber = (min,max) => {
-                    return Math.floor(Math.random() * (max - min + 1) + min);
-                }
-
-                const genenerateRandomPositionFood = (min,max) => {
-                    let x = getRandomNumber(min,max);
-                    let y = getRandomNumber(min,max);
-                    while (x===positionSquareX && y === positionSquareY)  {
-                        x = getRandomNumber(min,max);
-                        y = getRandomNumber(min,max);
-                    }
-                    setPositionFoodX(x);
-                    setPositionFoodY(y);
-                }
-
-                useEffect(()=>{
-                    if(isCollidedWidhFood) {
-                        genenerateRandomPositionFood(0,400);
-                        setCountFood(prev=>prev+1);
-                        setIsCollidedWidthFood(false);
-                    }
-                },[isCollidedWidhFood]);
-
-                useEffect(() => {
-
-                    if(stopMove) {
-                        return () => clearInterval(intervalId && intervalId );
-                    }
-                    const intervalId = setInterval(() => {
-                        if (keyDown) {
-                            setPositionSquareY(prevY =>  prevY <= 666 ? prevY + 10 : prevY)
-                        } else if (keyLeft) {
-                            setPositionSquareX(prevX =>  prevX > 0 ? prevX - 10 : prevX);
-                        } else if (keyUp) {
-                            setPositionSquareY(prevY => prevY >  0 ?  prevY - 10 : prevY);
-                        } else if (keyRight) {
-                            setPositionSquareX(prevX => prevX <= 466 ? prevX + 10 : prevX);
-
+                        
+                    });
+                });
+            } else if (keyLeft) {
+                setSquareList(prevElementi => {
+                    return prevElementi.map(el => {
+                        if(el.playable) {
+                            return el.x  > 0 ? { ...el, x: el.x - 10 } : { ...el, x: el.x };
                         }
-
-                    }, 500);
-
-                    window.addEventListener('keydown', handleKeyDown);
-
-                    return () => {
-                        clearInterval(intervalId);
-                        window.removeEventListener('keydown', handleKeyDown);
-                    };
-                }, [keyLeft, keyDown, keyRight, keyUp, setPositionSquareX, setPositionSquareY,stopMove])
-
-                useEffect(()=>{
-                    squareIsCollidedWithFood();
-                },[positionFoodX,positionFoodY,positionSquareX,positionSquareY]);
-
-                useEffect(()=>{
-                    genenerateRandomPositionFood(0,400);
-                },[]);
-
-                return (
-                    <>
-                        <div tabIndex={0} className="container-game">
-                            <Square positionX={positionSquareX} positionY={positionSquareY} size={squareSize} />
-                            <Food positionX={positionFoodX} positionY={positionFoodY} size={foodSize}/>
-                        </div>
-                        <div>
-                            <p>food: {countFood}</p>
-                        </div>
-                    </>
-
-                )
+                        
+                    });
+                });
+            } else if (keyUp) {
+                setSquareList(prevElementi => {
+                    return prevElementi.map(el => {
+                        if(el.playable) {
+                            return el.y  > 0 ? { ...el, y: el.y - 10 } : { ...el, y: el.y };
+                        }
+                        
+                    });
+                });
+            } else if (keyRight) {
+                setSquareList(prevElementi => {
+                    return prevElementi.map(el => {
+                        if(el.playable) {
+                            return el.x < 466 ? { ...el, x: el.x + 10 } : { ...el, x: el.x };
+                        }
+                        
+                    });
+                });
             }
 
-            export default Game;
+        }, 500);
+
+        window.addEventListener('keydown', handleKeyDown);
+
+        return () => {
+            clearInterval(intervalId);
+            window.removeEventListener('keydown', handleKeyDown);
+        };
+    }, [keyLeft, keyDown, keyRight, keyUp, setPositionSquareX, setPositionSquareY, stopMove])
+
+    useEffect(() => {
+        squareIsCollidedWithFood();
+        console.log("aggiornamento posizione")
+    }, [positionFoodX, positionFoodY, squareList]);
+
+    useEffect(() => {
+        genenerateRandomPositionFood(0, 400);
+    }, []);
+
+    return (
+        <>
+            <div tabIndex={0} className="container-game">
+                <Food positionX={positionFoodX} positionY={positionFoodY} size={foodSize} />
+                {squareList &&
+                    squareList.map(val => (
+                        <Square positionX={val.x} positionY={val.y} size={squareSize} />
+
+                    )
+                    )}
+            </div>
+            <div>
+                <p>food: {countFood}</p>
+            </div>
+        </>
+
+    )
+}
+
+export default Game;
